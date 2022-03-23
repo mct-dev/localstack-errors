87b1bfed69a8960f741051b20f1db8c50e87bcdb
Mike Tobias <mike.tobias@timebyping.com>
2022-03-23 14:36:42 -0700

    remove commented code

--- src/handlers.ts
+++ src/handlers.ts
@@ -5,26 +5,11 @@
  * exclusive rights under the Copyright Law of the U.S. (17 U.S.C. § 106) and may subject the
  * infringer thereof to severe legal liability.
  */
-// import 'reflect-metadata';
-// import './common/error-polyfill';
-
 import { Handler } from 'aws-lambda';
 import { v4 } from 'uuid';
-// import { ScheduledEvent, ScheduledHandler } from 'aws-lambda';
-
-// import { DependencyContainer } from './common/dependency-container';
-// import { ExportSchedulerLambdaHandler } from './lambda-handlers/export-scheduler-lambda-handler';
-// import { LambdaHandler } from './lambda-handlers/lambda-handler';
-
-// const dependency = new DependencyContainer();
-
-// export const exportScheduler: ScheduledHandler = async (event, context) => {
 export const test: Handler = async (event, context) => {
-  // const handler = dependency.container.resolve<LambdaHandler<ScheduledEvent, void>>(
-  //   ExportSchedulerLambdaHandler
-  // );
-  // return await handler.execute(event, context);
   console.log(v4());
+
   return {
     statusCode: 200,
     body: JSON.stringify({
